955fe0bc8e36261e0259c627de5745aaf23e8746
artbtjr <artbtjr@gmail.com>
2015-01-27 05:31:47 -0800

    working number pad and tile selection

--- sudoku.js
+++ sudoku.js
@@ -1,8 +1,6 @@
-var grid;
-
 function generateSudoku() {
 
-	grid = [
+	var grid = [
 		[1, 2, 3, 4, 5, 6, 7, 8, 9], 
 		[4, 5, 6, 7, 8, 9, 1, 2, 3], 
 		[7, 8, 9, 1, 2, 3, 4, 5, 6], 
@@ -14,57 +12,84 @@ function generateSudoku() {
 		[9, 1, 2, 3, 4, 5, 6, 7, 8]
 		];
 
+	var hGrid = [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+		[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+		[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+		[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+		[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+		[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+		[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+		[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+		[0, 0, 0, 0, 0, 0, 0, 0, 0]
+		];
+
+
 	shuffle(grid);
-	return grid;
+	hideTiles(grid, hGrid);
+	this.getTileNumber = function(row, col) {
+		return hGrid[row][col];
+	};
+
+	this.getSolution = function(row, col) {
+		return grid[row][col];
+	};
+
+	this.setNumber = function(row, col, num) {
+		grid[row][col] = num;
+	};
 }
 
 function shuffle(grid) {
 
+	var i, j, k, temp, col, col1, col2,
+	row1, row2, sub, sub1, sub2, num1, num2;
+
 	//swap the same columns of each subsquare
-	for(var i = 0; i < 25; i++) {
-		var col = Math.floor(Math.random()*3);
-		var sub1 = Math.floor(Math.random()*3);
-		var sub2 = Math.floor(Math.random()*3);
-		for(var j = 0; j < grid.length; j++) {
-			var temp = grid[j][col + sub1*3];
+	for(i = 0; i < 25; i++) {
+		col = Math.floor(Math.random()*3);
+		sub1 = Math.floor(Math.random()*3);
+		sub2 = Math.floor(Math.random()*3);
+		for(j = 0; j < grid.length; j++) {
+			temp = grid[j][col + sub1*3];
 			grid[j][col + sub1*3] = grid[j][col + sub2*3];
 			grid[j][col + sub2*3] = temp;
 		}
 	}
 
 	//swap all columns within each subsquare
-	for(var i = 0; i < 25; i++) {
-		var sub = Math.floor(Math.random()*3);
-		var col1 = Math.floor(Math.random()*3);
-		var col2 = Math.floor(Math.random()*3);
+	for(i = 0; i < 25; i++) {
+		sub = Math.floor(Math.random()*3);
+		col1 = Math.floor(Math.random()*3);
+		col2 = Math.floor(Math.random()*3);
 		while(col1 == col2) col2 = Math.floor(Math.random()*3);
-		for(var j = 0; j < grid.length; j++) {
-			var temp = grid[j][sub*3 + col1];
+		for(j = 0; j < grid.length; j++) {
+			temp = grid[j][sub*3 + col1];
 			grid[j][sub*3 + col1] = grid[j][sub*3 + col2];
 			grid[j][sub*3 + col2] = temp;
 		}
 	}
 
 	//swap all rows within each subsquare
-	for(var i = 0; i < 25; i++) {
-		var sub = Math.floor(Math.random()*3);
-		var row1 = Math.floor(Math.random()*3);
-		var row2 = Math.floor(Math.random()*3);
+	for(i = 0; i < 25; i++) {
+		sub = Math.floor(Math.random()*3);
+		row1 = Math.floor(Math.random()*3);
+		row2 = Math.floor(Math.random()*3);
 		while(row1 == row2) row2 = Math.floor(Math.random()*3);
-		for(var j = 0; j < grid.length; j++) {
-			var temp = grid[sub*3 + row1][j];
+		for(j = 0; j < grid.length; j++) {
+			temp = grid[sub*3 + row1][j];
 			grid[sub*3 + row1][j] = grid[sub*3 + row2][j];
 			grid[sub*3 + row2][j] = temp;
 		}
 	}
 
 	//swap one number with another
-	for(var i = 0; i < 25; i++) {
-		var num1 = Math.floor(Math.random()*9 + 1);
-		var num2 = Math.floor(Math.random()*9 + 1);
+	for(i = 0; i < 25; i++) {
+		num1 = Math.floor(Math.random()*9 + 1);
+		num2 = Math.floor(Math.random()*9 + 1);
 		while(num1 == num2) num2 = Math.floor(Math.random()*9 + 1);
-		for(var j = 0; j < grid.length; j++) {
-			for(var k = 0; k < grid[j].length; k++) {
+		for(j = 0; j < grid.length; j++) {
+			for(k = 0; k < grid[j].length; k++) {
 				if(grid[j][k] == num1)
 					grid[j][k] = num2;
 				else if(grid[j][k] == num2)
@@ -74,20 +99,102 @@ function shuffle(grid) {
 	}
 }
 
-function hideTiles(grid) {
+function hideTiles(aGrid, hiddenGrid) {
 
-	var hiddenGrid = grid;
+	// Randomly hide tiles, no guarantee for a unique solution
+	var numTiles, k;
 
-	//subsquare row
-	for (var i = 0; i < 3; i++) {
-		//subsquare column
-		for (var j = 0; j < 3; j++) {
-			numTiles = Math.floor(Math.random()*8 + 4);
+	for(var c = 0; c < 9; c++) {
+		for(var d = 0; d < 9; d++) {
+			hiddenGrid[c][d] = aGrid[c][d];
 		}
 	}
 
+	for(var i = 0; i < 4; i++) {
+		numTiles = Math.floor(Math.random()*8 + 6);
+		while(numTiles > 0) {
+			k = Math.floor(Math.random()*9);
+			hiddenGrid[i][k] = 0;
+			hiddenGrid[8-i][8-k] = 0;
+			numTiles--;
+			
+		}
+	}
+
+	numTiles = Math.floor(Math.random()*4 + 2);
+	while(numTiles > 0) {
+		k = Math.floor(Math.random()*4);
+		hiddenGrid[4][k] = 0;
+		hiddenGrid[4][8-k] = 0;
+		numTiles--;
+	}
 }
 
-function getTileNumber(row, column) {
-	return grid[row][column];
+	/*
+	for(var i = 0; i < 9; i++) {
+		for(var j = 0; j < 9; j++) {
+			var temp = grid[i][j];
+			grid[i][j] = "";
+			for(var k = 0; k < 9; k++) {
+				if( solver() ) {
+					var pos = [i][j];
+					hiddenGrid.push(pos);
+				}
+				else {// not unique
+					grid[i][j] = temp;
+				}
+			}
+		}
+	}
+
+	*/
+
+
+function solver(aGrid) {
+	for(var i = 0; i < 9; i++) {
+		for(var j = 0; j < 9; j++) {
+			if(aGrid[i][j] === 0) {
+				for(var k = 1; k < 10; k++) {
+					if(isValid(aGrid, i, j, k)) {
+						k = 10;
+					}
+					else {
+
+					}
+				}
+			}
+		}
+	}
+}
+
+function isValid(aGrid, row, col, num) {
+	if(aGrid[row].indexOf(num) == -1 && 
+		columnToArray(aGrid, col).indexOf(num) == -1 && 
+		subsquareToArray(aGrid, row, col).indexOf(num) == -1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function columnToArray(aGrid, col) {
+	var colArray;
+	for(var i = 0; i < 9; i++) {
+		colArray.push(aGrid[i][col]);
+	}
+	return colArray;
+}
+
+//Convert a subsquare to a 1D array, top left to bottom right
+function subsquareToArray(aGrid, row, col) {
+	var subArray;
+	var subrow = row - (row % 3);
+	var subcol = col - (col % 3);
+	for(var i = 0; i < 3; i++) {
+		for(var j = 0; j < 3; j++) {
+			subArray.push(aGrid[i+subrow][j+subcol]);
+		}
+	}
+	return subArray;
 }
